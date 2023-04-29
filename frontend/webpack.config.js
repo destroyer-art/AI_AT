eeb9db31cc7fb3ea7e206a037b837b1820d8d43f
DestroyerArt <175498058+destroyer-art@users.noreply.github.com>
2023-04-29 06:15:45 -0400

    Webpack conifg

---
+++ frontend/webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        port: 3000,
+    },
+};
