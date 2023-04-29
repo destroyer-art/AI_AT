7b4d70b0893a622c8fa737b81cbcb247260c14a3
DestroyerArt <175498058+destroyer-art@users.noreply.github.com>
2023-04-28 21:00:00 -0400

    Created PromptTempaltes, to create a youtube script, and then adjust it.

--- routes/main.py
+++ routes/main.py
@@ -2,18 +2,39 @@ from flask import Blueprint, render_template, request
 import os
 from apikey import apikey
 from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain, SequentialChain
 
 main_bp = Blueprint('main', __name__)
 
 os.environ["OPENAI_API_KEY"] = apikey
 
-llm = OpenAI(temperature=0.9)
+# Prompt template for LLM
+script_template = PromptTemplate(
+    input_variables=['topic'],
+    template='Write me a YouTube voiceover script about {topic}.',
+)
+
+adjust_template = PromptTemplate(
+    input_variables=['script'],
+    template='Adjust the script in a fun, relaxed way: {script}',
+)
+
+# LLMs
+llm = OpenAI(temperature=0.1)
+script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True, output_key='script')
+adjust_chain = LLMChain(llm=llm, prompt=adjust_template, verbose=True, output_key='adjust')
+sequential_chain = SequentialChain(chains=[script_chain, adjust_chain], input_variables=['topic'], output_variables=['script', 'adjust'], verbose=True)
 
 @main_bp.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         prompt = request.form.get('prompt')
-        generated_text = llm(prompt)
-        return render_template('index.html', generated_text=generated_text)
+
+        # Run the chain with the prompt
+        response = sequential_chain({'topic': prompt})  # Use 'topic' as the key
+        print(response['script'])  # Log the response to the console
+        print(response['adjust']) 
+        return render_template('index.html', generated_text=response)
     else:
         return render_template('index.html')
