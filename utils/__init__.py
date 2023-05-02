be2e626268cf688035f9767fc01e2bf2e4b4b24f
DestroyerArt <175498058+destroyer-art@users.noreply.github.com>
2023-05-02 17:12:35 -0400

    Added subtitle_utils to __init__

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,3 +2,4 @@ from .serpapi_utils import get_image_results
 from .polly_utils import synthesize_speech
 from .video_utils import create_video
 from .s3_utils import upload_to_s3
+from .subtitle_utils import split_sentences, generate_subtitle_timings
