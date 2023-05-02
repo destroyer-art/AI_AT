cc899d40a373c16305d7f4d7e8c48e6348573ec6
DestroyerArt <175498058+destroyer-art@users.noreply.github.com>
2023-05-02 04:33:40 -0400

    Corrected for right url

--- utils/s3_utils.py
+++ utils/s3_utils.py
@@ -8,11 +8,11 @@ AWS_SECRET_ACCESS_KEY = aws_secret_key
 BUCKET_NAME = 'videos.yt'
 
 s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
-
 def upload_to_s3(file_path, object_name):
     try:
         s3.upload_file(file_path, BUCKET_NAME, os.path.basename(object_name))
-        url = f"https://{BUCKET_NAME}.s3.amazonaws.com/{object_name}"
+        print(object_name, "uploaded to S3")
+        url = f"https://s3.amazonaws.com/{BUCKET_NAME}/{object_name.split('/')[-1]}"
         print("URL:", url)
         return url
     except FileNotFoundError:
