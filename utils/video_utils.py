1e2a2b647e243dc04d67777cb0d5cab12f34ba6f
DestroyerArt <175498058+destroyer-art@users.noreply.github.com>
2023-05-02 16:09:56 -0400

    Added resize_image function, now showing subtitles one word at a time ( Need to adjust ) but they are somewhat on time )

--- utils/video_utils.py
+++ utils/video_utils.py
@@ -1,89 +1,92 @@
-import moviepy.editor as mp
-import requests
-import base64
-import imageio
-import tempfile
 import os
-from moviepy.video.fx.all import resize
-from moviepy.video.VideoClip import TextClip
-from moviepy.video.compositing.CompositeVideoClip import CompositeVideoClip
+import moviepy.editor as mp
+from moviepy.editor import TextClip, CompositeVideoClip
+from moviepy.video.fx.resize import resize
+import requests
 from io import BytesIO
+from PIL import Image
+import base64
+import numpy as np
 from pathlib import Path
-from .s3_utils import upload_to_s3
+from utils.subtitle_utils import split_sentences, generate_subtitle_timings
+from utils.polly_utils import synthesize_speech
 
-def create_video(image_urls, audio_base64, text, output_file):
-    """Creates a video from a list of image URLs, an audio file, and some text.
+def resize_image(image, width=1280, height=720):
+    aspect_ratio = image.width / image.height
+    new_width = width
+    new_height = int(new_width / aspect_ratio)
 
-    Args:
-        image_urls: A list of image URLs.
-        audio_base64: The audio file as a base64-encoded string.
-        text: The text to be displayed on the video.
-        output_file: The name of the output file.
+    if new_height > height:
+        new_height = height
+        new_width = int(new_height * aspect_ratio)
 
-    Returns:
-        The video as a array buffer.
-    """
+    image = image.resize((new_width, new_height), Image.ANTIALIAS)
+    return image
 
+def create_video(image_urls, audio_base64, script, output_file):
     clips = []
-
-    # Download and process images
+    sentences = split_sentences(script)
+    subtitle_timings = generate_subtitle_timings(sentences, synthesize_speech)
+    
+    background = Image.new('RGB', (1280, 720), color='black')
+    
     for url in image_urls:
+        print(f"Processing image: {url['thumbnail']}")
         image_data = url['thumbnail']
         response = requests.get(image_data)
-        image = mp.ImageClip(imageio.imread(response.content))
-        image = resize(image, width=1280)
-        clips.append(image.set_duration(5))
+        image = Image.open(BytesIO(response.content))
+        image = resize_image(image, width=1280, height=720)
+        img_bg = background.copy()
+        img_bg.paste(image, (int((background.width - image.width) / 2), int((background.height - image.height) / 2)))
+        img_bg_clip = mp.ImageClip(np.array(img_bg)).set_duration(5)
+        clips.append(img_bg_clip)
 
-    # Concatenate images
     concatenated_clip = mp.concatenate_videoclips(clips)
 
-    # Create subtitles
-    subtitles = TextClip(
-        text,
-        fontsize=24,
-        color='white',
-        size=(concatenated_clip.w, 100),
-        bg_color='black',
-        print_cmd=True
-    ).set_position(('center', 'bottom')).set_duration(concatenated_clip.duration)
 
-    # Create composite clip
+    subtitle_clips = []
+    for start, end, sentence in subtitle_timings:
+        if not sentence.strip():  # skip empty sentences
+            continue
+
+        print(f"Creating subtitle clip: start={start}, end={end}, sentence={sentence}")
+        subtitle_clip = TextClip(
+            sentence,
+            fontsize=24,
+            color='white',
+            size=(concatenated_clip.w, 100),
+            bg_color='black',
+            print_cmd=True
+        ).set_position(('center', 'bottom')).set_start(start).set_end(end)
+        subtitle_clips.append(subtitle_clip)
+
+    subtitles = CompositeVideoClip(subtitle_clips)
     composite_clip = CompositeVideoClip([concatenated_clip, subtitles])
 
     temp_dir = Path(__file__).resolve().parent / 'temp'
     temp_dir.mkdir(parents=True, exist_ok=True)
 
-    # Save audio_base64 content to a temporary file
     audio_data = base64.b64decode(audio_base64)
     audio_temp_file = temp_dir / 'temp_audio.wav'
     with open(audio_temp_file, 'wb') as f:
         f.write(audio_data)
 
-    # Use the temporary file path for mp.AudioFileClip
     audioclip = mp.AudioFileClip(str(audio_temp_file))
 
-    # Set the audio for the composite clip
     final_clip = composite_clip.set_audio(audioclip)
 
-    # Get the absolute path to the output file
     output_path = temp_dir / 'video.mp4'
 
-    # Write the video to the specified output file
     temp_audio_path = temp_dir / 'temp_audio.m4a'
     final_clip.write_videofile(str(output_path), codec='libx264', temp_audiofile=str(temp_audio_path), remove_temp=False, audio_codec='aac', fps=24)
 
-
-    # Read the video file as an ArrayBuffer
     with open(output_file, 'rb') as f:
         video_data = f.read()
 
-    # Remove the temporary audio file
     if audio_temp_file.exists():
         audio_temp_file.unlink()
 
     if temp_audio_path.exists():
         temp_audio_path.unlink()
-
-
 
     return video_data
