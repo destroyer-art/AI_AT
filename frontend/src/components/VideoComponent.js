6a83dc9ce87a256804a9cb38362e2adf8599dc49
DestroyerArt <175498058+destroyer-art@users.noreply.github.com>
2023-05-03 00:53:28 -0400

    Set-up Videojs for more options to customize player in future, as well as better compatability across browsers I am hoping lol.

--- frontend/src/components/VideoComponent.js
+++ frontend/src/components/VideoComponent.js
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import videojs from 'video.js';
+import 'video.js/dist/video-js.min.css';
 
 const VideoComponent = ({ videoSrc, generatedText, imageResults, audioBase64, setVideoSrc }) => {
+    const videoRef = useRef();
+
+    useEffect(() => {
+        if (videoRef.current && videoSrc) {
+            const player = videojs(videoRef.current, {
+                autoplay: true,
+                controls: true,
+            });
+
+            player.src({ type: 'video/mp4', src: videoSrc });
+
+            return () => {
+                player.dispose();
+            };
+        }
+    }, [videoSrc]);
+
 
     const fetchGeneratedVideo = async () => {
         const response = await fetch("http://localhost:5000/api/video", {
@@ -34,11 +53,11 @@ const VideoComponent = ({ videoSrc, generatedText, imageResults, audioBase64, se
 
     return (
         <div>
+            <div data-vjs-player>
+                <video ref={videoRef} className="video-js" />
+            </div>
             {videoSrc ? (
-                <video controls>
-                    <source src={videoSrc} type="video/mp4" />
-                    Your browser does not support the video tag.
-                </video>
+                <p>Video is available. Press play to watch.</p>
             ) : (
                 <p>No video available.</p>
             )}
