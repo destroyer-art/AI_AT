3f22409772179f803f62ceda9bbe7ca62b56e437
DestroyerArt <175498058+destroyer-art@users.noreply.github.com>
2023-05-03 00:49:42 -0400

    Updated how many images are used

--- utils/serpapi_utils.py
+++ utils/serpapi_utils.py
@@ -1,8 +1,9 @@
 from serpapi import GoogleSearch
 from apikey import serp
 
+
 def get_image_results(query):
-    num_results = 5  # Set the fixed value for the number of result images
+    num_results = 10  # Set the fixed value for the number of result images
     params = {
         "q": query,
         "engine": "google_images",
@@ -12,7 +13,9 @@ def get_image_results(query):
 
     search = GoogleSearch(params)
     results = search.get_dict()
-    images_results = results["images_results"][:num_results]  # Slice the list to get the desired number of results
+    images_results = results["images_results"][
+        :num_results
+    ]  # Slice the list to get the desired number of results
 
     print(f"Number of image results: {len(images_results)}")
 
