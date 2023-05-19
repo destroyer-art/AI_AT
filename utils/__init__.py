2fc760c2c6e44df8dcdceb6876ab3dc810d6f50a
DestroyerArt <175498058+destroyer-art@users.noreply.github.com>
2023-05-19 17:41:00 -0400

    Added celery config

--- utils/__init__.py
+++ utils/__init__.py
@@ -4,3 +4,4 @@ from .video_utils import create_video
 from .s3_utils import upload_to_s3
 from .subtitle_utils import split_sentences, generate_subtitle_timings
 from .unsplash_utils import get_unsplash_image_urls
+from .progress_utils import celery
