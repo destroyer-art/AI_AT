0e462a7bc72896646b962e0b5ec8b6c8a4d6f024
DestroyerArt <175498058+destroyer-art@users.noreply.github.com>
2023-05-19 17:41:27 -0400

    Refactored config

--- utils/progress_utils.py
+++ utils/progress_utils.py
@@ -4,4 +4,4 @@ import time
 celery = Celery("app", broker='redis://localhost:6379/0', backend='redis://localhost:6379/0')
 celery.conf.update(task_track_started=True)
 
-from utils.video_utils import create_video
+
